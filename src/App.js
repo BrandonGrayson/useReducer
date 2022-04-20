c140a87cad2758470f84afc45d6735996d0b0132
Brandon <brandonbgrayson@gmail.com>
2022-04-20 08:59:47 -0400

    useReducer is able to increment by 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useReducer } from "react";
 
+function reducer(state, action) {
+  return state += action
+}
 
 function App() {
+  const [state, dispatch] = useReducer(
+    reducer,
+    0
+  )
   return (
     <div >
+      <p>{state}</p>
 
+      <button onClick={() => {dispatch(1)}}>Add 1</button>
     </div>
   );
 }
