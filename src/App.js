be13d1ba77d71ed520c0f0ea4118e7111faadba7
Brandon <brandonbgrayson@gmail.com>
2022-04-20 12:16:52 -0400

    able to increment count by the step

--- src/App.js
+++ src/App.js
@@ -1,37 +1,60 @@
 import { useReducer } from "react";
 
-function reducer(state, action) {
-  switch (action) {
-    case 'increment':
-      return state += 1
-    case 'decrement':
-      return state -= 1
-    case 'reset':
-      return state = 0
-      default:
-        return state
-  }
+// function reducer(state, action) {
+//   console.log(action.type)
+//   switch (action.type) {
+    
+//     case 'increment':
+//       return {count: state.count + state.step, step: state.step}
+//     case 'decrement':
+//       return {count: state.count - state.step, step: state.step}
+//     case 'reset':
+//       return {count: state.count = 0, step: state.step}
+//     case 'updateStep':
+//       return {count: state.count, step: action.step}
+//       default:
+//         return state
+//   }
 
+// }
+
+function reducer (state, action) {
+  if (action.type === "increment") {
+    return {count: parseInt(state.count) + parseInt(state.step), step: state.step}
+  } else if (action.type === "decrement") {
+    return {count: state.count - state.step, step: state.step}
+  } else if (action.type === "updateStep") {
+    return {count: state.count, step: action.step}
+  } else if (action.type === "reset") {
+    return {count: state.count = 0, step: state.step}
+  } else {
+    throw new Error()
+  }
 }
 
 function App() {
+
   const [state, dispatch] = useReducer(
     reducer,
-    0
+    {count: 0, step: 1}
   )
+
+  console.log('count', state.count)
+  console.log('step', state.step)
   return (
     <div >
       <input 
        type="range" 
        min="1" 
        max="10"
-       onChange={(e) => console.log(e.target.value)}/>
+       onChange={(e) => dispatch({type: 'updateStep', step: e.target.value})}/>
 
-      <p>{state}</p>
+      <p>{state.count} </p>
 
-      <button onClick={() => {dispatch('increment')}}>Add 1</button>
-      <button onClick={() => dispatch('decrement')}>Subtract 1</button>
-      <button onClick={() => dispatch('reset')}>reset</button>
+      <button onClick={() => dispatch({type: 'increment'})}>Add </button>
+      <button onClick={() => dispatch({type: 'decrement'})}>Subtract</button>
+      <button onClick={() => dispatch({type: 'reset'})}>reset</button>
+      <p>{state.step}</p>
     </div>
   );
 }
